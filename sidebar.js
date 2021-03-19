4d3516889d0d1f3320ba9b0beb0ecee7075c156f
Robert Hernandez <semi.truck.colon@gmail.com>
2021-03-19 13:05:58 -0700

    more updated look

--- sidebar.js
+++ sidebar.js
@@ -1,5 +1,6 @@
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
+  document.getElementById("mySidebarButton").style.visibility = 'hidden';
   document.getElementById("mySidebar").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
 }
@@ -8,4 +9,5 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("mySidebarButton").style.visibility = 'visible';
 }
