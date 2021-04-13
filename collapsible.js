28a3340180fe98eb8a2efbf61f9d7d13be4f9bae
Robert Hernandez <semi.truck.colon@gmail.com>
2021-04-12 20:56:39 -0500

    should be final draft. awaiting approval

--- collapsible.js
+++ collapsible.js
@@ -1,15 +1,33 @@
- 
-  var coll = document.getElementsByClassName("collapsible");
-  var i;
-
-  for (i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function() {
-      this.classList.toggle("active");
-      var content = this.nextElementSibling;
-      if (content.style.maxHeight){
-        content.style.maxHeight = null;
-      } else {
-        content.style.maxHeight = content.scrollHeight + "px";
-      }
-    });
+  // toggle collapse of specified content
+  function toggleContent(content) {
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + 'px';
+    }
   }
+
+// collapse all open content
+function collapseAllOpenContent() {
+  const colls = document.getElementsByClassName('collapsible');
+  for (const coll of colls) {
+    if (coll.classList.contains('active')) {
+      coll.classList.remove('active');
+      toggleContent(coll.nextElementSibling);
+    }
+  }
+}
+
+
+ const coll = document.getElementsByClassName("collapsible");
+ var i;
+
+ for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+    if(!this.classList.contains('active')) {
+      collapseAllOpenContent();
+    }
+    this.classList.toggle("active");
+    toggleContent(this.nextElementSibling);
+    });
+ }
